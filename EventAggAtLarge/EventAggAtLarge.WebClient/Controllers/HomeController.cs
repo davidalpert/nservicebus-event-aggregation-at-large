b496abb04f4aed9e4774911634c7571e43801306
David Alpert <david@spinthemoose.com>
2011-06-27 13:13:35 -0500

    styling the web content to replace the ASP.NET MVC default brand with a more talk-appropriate one... might have to use this site for my slides...

--- EventAggAtLarge/EventAggAtLarge.WebClient/Controllers/HomeController.cs
+++ EventAggAtLarge/EventAggAtLarge.WebClient/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace EventAggAtLarge.WebClient.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
 
