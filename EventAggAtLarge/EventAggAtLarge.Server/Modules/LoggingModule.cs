100a7a6e080e333a05b3022452f90ae277aa8da1
David Alpert <david@spinthemoose.com>
2011-06-29 16:09:51 -0500

    adding a sample IMessageModule

---
+++ EventAggAtLarge/EventAggAtLarge.Server/Modules/LoggingModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventAggAtLarge.Server.Modules
+{
+    // NHibernate session management in NServiceBus by Andreas Ohlund
+    // http://andreasohlund.net/2010/02/03/nhibernate-session-management-in-nservicebus/
+
+    public class LoggingModule : NServiceBus.IMessageModule
+    {
+        public void HandleBeginMessage()
+        {
+            Console.WriteLine("HandleBeginMessage ------------------------------------------------");
+        }
+
+        public void HandleEndMessage()
+        {
+            Console.WriteLine("HandleEndMessage --------------------------------------------------");
+        }
+
+        public void HandleError()
+        {
+            Console.WriteLine("HandleError -------------------------------------------------------");
+        }
+    }
+}
